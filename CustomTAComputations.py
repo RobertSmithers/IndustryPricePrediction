894ca01602fc1cada5a02fc2eebd39052c98c420
E2023 <yiliart1223@gmail.com>
2022-03-21 10:23:56 -0400

    changed to csv

--- CustomTAComputations.py
+++ CustomTAComputations.py
@@ -6,6 +6,7 @@ import matplotlib.dates as mdates
 from pandas.plotting import scatter_matrix
 import yfinance as yf
 import talib
+#import talib
 #%matplotlib inline
 
 start = '2017-02-19'
@@ -13,8 +14,10 @@ end = '2022-2-19'
 sp500 = yf.download('^GSPC', start, end)
 
 # Moving Averages https://www.analyticsvidhya.com/blog/2021/07/stock-prices-analysis-with-python/#h2_5
-sp500['MA50'] = sp500['Close'].rolling(50).mean()
-sp500['MA200'] = sp500['Close'].rolling(200).mean()
+def MA(data_frame, days):
+	name = 'MA'+str(days)
+	data_frame[name] = data_frame['close'].rolling(days).mean()
+	return data_frame
 
 # RSI https://wire.insiderfinance.io/calculate-rsi-with-python-and-yahoo-finance-c8fb78b1c199
 def RSI(data, window = 14, adjust = False):
@@ -33,7 +36,7 @@ def RSI(data, window = 14, adjust = False):
 
     return RSI
 reversed_df = sp500.iloc[::-1]
-sp500['RSI'] = talib.RSI(reversed_df['Close'], 14)
+#sp500['RSI'] = talib.RSI(reversed_df['Close'], 14)
 
 locator = mdates.MonthLocator(interval = 3)
 fmt = mdates.DateFormatter('%b')
@@ -106,6 +109,79 @@ for i in range(0, len(Dvalue) - dperiods + 1):
 	Jvalue.append(j)
 sp500['J'] = pd.Series(Jvalue)
 
+# SP500
+sp500_data = pd.read_csv('~/Documents/Github/IndustryPricePrediction/data/sectors/SP500_7yr_daily.csv')
+sp500_frame = pd.DataFrame(sp500_data, columns = ['ticker', 'descr', 'date', 'close', 'retx'])
+MA(sp500_frame, 50)
+MA(sp500_frame, 200)
+
+# XLB
+xlb_data = pd.read_csv('~/Documents/Github/IndustryPricePrediction/data/sectors/XLB_7yr_daily.csv')
+xlb_frame = pd.DataFrame(xlb_data, columns = ['ticker', 'descr', 'date', 'low', 'high', 'close', 'vol', 'ret', 'bid', 'ask', 'retx'])
+MA(xlb_frame, 50)
+MA(xlb_frame, 200)
+
+# XLC
+xlc_data = pd.read_csv('~/Documents/Github/IndustryPricePrediction/data/sectors/XLC_7yr_daily.csv')
+xlc_frame = pd.DataFrame(xlc_data, columns = ['ticker', 'descr', 'date', 'low', 'high', 'close', 'vol', 'ret', 'bid', 'ask', 'retx'])
+MA(xlc_frame, 50)
+MA(xlc_frame, 200)
+
+# XLE
+xle_data = pd.read_csv('~/Documents/Github/IndustryPricePrediction/data/sectors/XLE_7yr_daily.csv')
+xle_frame = pd.DataFrame(xle_data, columns = ['ticker', 'descr', 'date', 'low', 'high', 'close', 'vol', 'ret', 'bid', 'ask', 'retx'])
+MA(xle_frame, 50)
+MA(xle_frame, 200)
+
+# XLF
+xlf_data = pd.read_csv('~/Documents/Github/IndustryPricePrediction/data/sectors/XLF_7yr_daily.csv')
+xlf_frame = pd.DataFrame(xlf_data, columns = ['ticker', 'descr', 'date', 'low', 'high', 'close', 'vol', 'ret', 'bid', 'ask', 'retx'])
+MA(xlf_frame, 50)
+MA(xlf_frame, 200)
+
+# XLI
+xli_data = pd.read_csv('~/Documents/Github/IndustryPricePrediction/data/sectors/XLI_7yr_daily.csv')
+xli_frame = pd.DataFrame(xli_data, columns = ['ticker', 'descr', 'date', 'low', 'high', 'close', 'vol', 'ret', 'bid', 'ask', 'retx'])
+MA(xli_frame, 50)
+MA(xli_frame, 200)
+
+# XLK
+xlk_data = pd.read_csv('~/Documents/Github/IndustryPricePrediction/data/sectors/XLK_7yr_daily.csv')
+xlk_frame = pd.DataFrame(xlk_data, columns = ['ticker', 'descr', 'date', 'low', 'high', 'close', 'vol', 'ret', 'bid', 'ask', 'retx'])
+MA(xlk_frame, 50)
+MA(xlk_frame, 200)
+
+# XLP
+xlp_data = pd.read_csv('~/Documents/Github/IndustryPricePrediction/data/sectors/XLP_7yr_daily.csv')
+xlp_frame = pd.DataFrame(xlp_data, columns = ['ticker', 'descr', 'date', 'low', 'high', 'close', 'vol', 'ret', 'bid', 'ask', 'retx'])
+MA(xlp_frame, 50)
+MA(xlp_frame, 200)
+
+# XLRE
+xlre_data = pd.read_csv('~/Documents/Github/IndustryPricePrediction/data/sectors/XLRE_7yr_daily.csv')
+xlre_frame = pd.DataFrame(xlre_data, columns = ['ticker', 'descr', 'date', 'low', 'high', 'close', 'vol', 'ret', 'bid', 'ask', 'retx'])
+MA(xlre_frame, 50)
+MA(xlre_frame, 200)
+
+# XLU
+xlu_data = pd.read_csv('~/Documents/Github/IndustryPricePrediction/data/sectors/XLU_7yr_daily.csv')
+xlu_frame = pd.DataFrame(xlu_data, columns = ['ticker', 'descr', 'date', 'low', 'high', 'close', 'vol', 'ret', 'bid', 'ask', 'retx'])
+MA(xlu_frame, 50)
+MA(xlu_frame, 200)
+
+# XLV
+xlv_data = pd.read_csv('~/Documents/Github/IndustryPricePrediction/data/sectors/XLV_7yr_daily.csv')
+xlv_frame = pd.DataFrame(xlv_data, columns = ['ticker', 'descr', 'date', 'low', 'high', 'close', 'vol', 'ret', 'bid', 'ask', 'retx'])
+MA(xlv_frame, 50)
+MA(xlv_frame, 200)
+
+# XLY
+xly_data = pd.read_csv('~/Documents/Github/IndustryPricePrediction/data/sectors/XLY_7yr_daily.csv')
+xly_frame = pd.DataFrame(xly_data, columns = ['ticker', 'descr', 'date', 'low', 'high', 'close', 'vol', 'ret', 'bid', 'ask', 'retx'])
+MA(xly_frame, 50)
+MA(xly_frame, 200)
+
+
 ax1 = plt.subplot2grid((3, 3), (0, 0), colspan = 3)
 ax2 = plt.subplot2grid((3, 3), (1, 0), colspan = 3)
 ax3 = plt.subplot2grid((3, 3), (2, 0), colspan = 3)
@@ -119,11 +195,13 @@ ax1.legend()
 #X1.set_major_locator(locator)
 #X1.set_major_formatter(fmt)
 
+""""
 ax2.set_title('RSI of S&P 500, 2/19/2017 - 2/18/2022')
 ax2.plot(sp500['RSI'], color = 'red', linewidth = 1.5, label = "RSI")
 ax2.axhline(30, linestyle='--', linewidth=1, color='grey')
 ax2.axhline(70, linestyle='--', linewidth=1, color='grey')
-ax1.legend()
+ax2.legend()
+"""
 
 ax3.set_title('KDJ Indicator')
 ax3.plot(Kvalue, linewidth = 0.2, color = 'red', label = "%K line")
